e6ed17d63e98c5c9607d818622c1fc04b16dac67
Deztiwanolita <134885051+Deztiwanolita@users.noreply.github.com>
2025-10-14 16:48:20 -0300

    p
    p

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,21 @@
 extends Area2D
+signal hit
 
 @export var speed = 400
 var screen_size
 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	hide()
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -23,6 +30,22 @@ func _process(delta):
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+		
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
